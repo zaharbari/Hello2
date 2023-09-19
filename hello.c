f4d4e46ca0e022cb770e2800c914772a3365b618
zaharbari <zbari2@huskers.unl.edu>
2023-09-19 11:02:44 -0700

    initial commit

---
+++ hello.c
@@ -0,0 +1,14 @@
+/**
+ * Author: Zahar Bari
+ * Email: zbari2@huskers.unl.edu
+ * Date: 2023/09/19
+ *
+ * code that prints your name and major
+*/
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+    printf("My name is Zahar Bari!\n");
+}
